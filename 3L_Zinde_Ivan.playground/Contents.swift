f9da3bbc473290e4509cc0889f47f3f4d4e423dc
ZindeIvan <zinde.ivan@yandex.ru>
2020-06-07 13:52:12 +0600

    init struct

--- 3L_Zinde_Ivan.playground/Contents.swift
+++ 3L_Zinde_Ivan.playground/Contents.swift
@@ -1,3 +1,25 @@
 import Foundation
 
+enum Manufacturer : String{
+    case toyota = "Toyota"
+    case ford   = "Ford"
+    case honda  = "Honda"
+}
 
+struct Car {
+    let manufacturer : Manufacturer
+    let modelYear : UInt
+    let trunkMaxCapacity : Double
+    var engineOn : Bool
+    var windowsAreOpen : Bool
+    var trunkCurrentCapasity : Double
+}
+
+struct Truck {
+    let manufacturer : Manufacturer
+    let modelYear : UInt
+    let bodyMaxCapacity : Double
+    var engineOn : Bool
+    var windowsAreOpen : Bool
+    var bodyCurrentCapasity : Double
+}
